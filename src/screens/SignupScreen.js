763e90407135d888060ad397f6214e63d2554af9
Carl Puerschner <clawrence04@gmail.com>
2019-12-23 12:52:24 -0700

    Add files via upload

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
+import { nodeInternals } from 'stack-utils';
+
+const SignupScreen = ({ navigation }) => {
+    return (
+        <>
+           <Spacer>
+           <Text h3>Sign Up for Tracker</Text>
+            </Spacer>
+            <Input label="Email" />
+           <Spacer />
+            <Input label="Password" />
+           <Spacer>
+            <Button title="Sign Up" />
+           </Spacer>
+        </>
+    );
+};
+
+
+const styles = StyleSheet.create({});
+
+export default SignupScreen;
